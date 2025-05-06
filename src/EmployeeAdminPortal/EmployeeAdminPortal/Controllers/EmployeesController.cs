ccaef164796e2cc4b70f87f9f96c7af224102f19
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2025-05-06 13:36:43 -0400

    feat(Project): add basic controller with hardcoded return

---
+++ src/EmployeeAdminPortal/EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeAdminPortal.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase {
+
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> GetAllEmployees() {
+            var employees = new List<Employee> {
+                new Employee {
+                    Id = Guid.NewGuid(),
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Email = "JohnDoe@email.com",
+                    PhoneNumber = "123-456-7890",
+                    Salary = 50000
+                }
+            };
+
+            return Ok(employees);
+        }
+    }
+}
