58d2fa60e81510bfc1957921fc2e0b62d1c40929
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2025-05-06 22:04:54 -0400

    chore(Database): do initial database migration

--- src/EmployeeAdminPortal/EmployeeAdminPortal/Program.cs
+++ src/EmployeeAdminPortal/EmployeeAdminPortal/Program.cs
@@ -1,3 +1,6 @@
+using EmployeeAdminPortal.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,11 +10,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
