45d486230daf20b6d5076a3df9183d65816b01fd
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2025-05-08 13:31:31 -0400

    feat: add Product entity and hardcoded return for GET "/products" endpoint

---
+++ src/ProductInventoryAPI/ProductInventoryAPI/Models/Entities/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductInventoryAPI.Models.Entities {
+    public class Product {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public int quantity { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
--- src/ProductInventoryAPI/ProductInventoryAPI/Program.cs
+++ src/ProductInventoryAPI/ProductInventoryAPI/Program.cs
@@ -1,6 +1,18 @@
+using ProductInventoryAPI.Models.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/products", () => {
+    var products = new List<Product>
+    {
+        new Product { Id = Guid.NewGuid(), Name = "Product 1", Price = 10.0m },
+        new Product { Id = Guid.NewGuid(), Name = "Product 2", Price = 20.0m },
+        new Product { Id = Guid.NewGuid(), Name = "Product 3", Price = 30.0m }
+    };
+    return products;
+});
 
 app.Run();
