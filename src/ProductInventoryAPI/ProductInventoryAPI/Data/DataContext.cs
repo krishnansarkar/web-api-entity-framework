2144180f7955c3680a0398761930612a8745a226
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2025-05-08 13:58:36 -0400

    feat: add mssql db connection

---
+++ src/ProductInventoryAPI/ProductInventoryAPI/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductInventoryAPI.Models.Entities;
+
+namespace ProductInventoryAPI.Data {
+    public class DataContext : DbContext {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- src/ProductInventoryAPI/ProductInventoryAPI/Program.cs
+++ src/ProductInventoryAPI/ProductInventoryAPI/Program.cs
@@ -1,18 +1,17 @@
-using ProductInventoryAPI.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using ProductInventoryAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/products", () => {
-    var products = new List<Product>
-    {
-        new Product { Id = Guid.NewGuid(), Name = "Product 1", Price = 10.0m },
-        new Product { Id = Guid.NewGuid(), Name = "Product 2", Price = 20.0m },
-        new Product { Id = Guid.NewGuid(), Name = "Product 3", Price = 30.0m }
-    };
-    return products;
+app.MapGet("/products", async (DataContext db) => {
+    return await db.Products.ToListAsync();
 });
 
 app.Run();
